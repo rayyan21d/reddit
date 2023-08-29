88c74822e33ecca97b9e9e8591d83716fba56c74
Phoenix2157 <69739582+rayyan21d@users.noreply.github.com>
2023-08-29 10:51:38 +0530

    Update route.ts

--- src/app/api/subreddit/route.ts
+++ src/app/api/subreddit/route.ts
@@ -1,18 +1,18 @@
-// import { getAuthSession } from "@/lib/auth";
-// import { SubredditSubscriptionValidator } from "@/lib/validators/subreddit";
+import { getAuthSession } from "@/lib/auth";
+import { SubredditSubscriptionValidator } from "@/lib/validators/subreddit";
 
-// export async function POST(req: Request){
-//     try{
-//         const session = await getAuthSession()
+export async function POST(req: Request){
+    try{
+        const session = await getAuthSession()
 
-//         if (!session?.user){
-//             return new Response("Unauthorized", {status: 401})
-//         }
+        if (!session?.user){
+            return new Response("Unauthorized", {status: 401})
+        }
 
-//         const body = await req.json()
-//         const {name} = SubredditSubscriptionValidator.parse(body)
+        const body = await req.json()
+        const {name} = SubredditSubscriptionValidator.parse(body)
 
 
-//     }
-//     catch (err){}
-// }
+    }
+    catch (err){}
+}
